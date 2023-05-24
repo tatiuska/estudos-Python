bf07710f67a3874b90fd6cb94a4e40ca23403dad
tatiuska <tfweska@gmail.com>
2023-05-23 23:38:26 -0300

    exercícios operadores e expressões matemáticas

---
+++ ex001.py
@@ -0,0 +1,12 @@
+#Exercícios página 44 (2.1 e 2.2)
+#2.1: Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python
+print("Expressão 1:")
+print("10 + 20 * 30 =", 10+20*30)
+print("Expressão 2:")
+print("4 ** 2 / 30 =", 4**2/30)
+print("Expressão 3:")
+print("(9 ** 4 + 2) * 6 - 1", (9**4+2)*6-1)
+
+#2.2: Digite a seguinte expressão no interpretador
+print("Expressão 4:")
+print("10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2 =", 10%3*10**2+1-10*4/2)
