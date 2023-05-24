0b97d0dba2d40bb0f43ae42745637ff69a04502e
tatiuska <tfweska@gmail.com>
2023-05-24 16:54:50 -0300

    exercício operadores relacionais

---
+++ ex005.py
@@ -0,0 +1,8 @@
+#Exercício 3.4: Escreva uma expressão para determinar se uma pessoa deve ou não pagar imposto
+#Considere que pagam imposto pessoas cujo salário é maior que R$ 1200,00
+#Variáveis
+salario = 5000
+if(salario >= 1200):
+    print("Seu salário é de R$", salario, ". Paga imposto!")
+else:
+    print("Seu salário é de R$", salario, ". Não paga imposto!")
