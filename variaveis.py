4c1d825d159522488f4f5ddec9aad64f8171d41f
tatiuska <tfweska@gmail.com>
2023-05-24 00:01:28 -0300

    práticas com variáveis

---
+++ variaveis.py
@@ -0,0 +1,13 @@
+#Conceitos de variáveis e atribuição
+#Listagem 2.7: Primeiro programa com variáveis
+a = 2
+b = 3
+print("a = 2, b = 3")
+print("a + b =", a + b)
+
+#Listagem 2.11: Cálculo de aumento de salário
+salario = 1500
+aumento = 5
+print("salário = 1500, aumento = 5")
+print("salario + (salario * aumento / 100) =", salario + (salario * aumento / 100))
+
