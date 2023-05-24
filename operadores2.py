749c045502d0b276e5a53af2aa7ccfa920126b64
tatiuska <tfweska@gmail.com>
2023-05-24 15:42:28 -0300

    exemplos de uso de operadores relacionais e lógicos

---
+++ operadores2.py
@@ -0,0 +1,23 @@
+#Operadores relacionais
+a = 1
+print("a recebe", a)
+b = 5
+print("b recebe", b)
+c = 2
+print("c recebe", c)
+d = 1
+print("d recebe", d)
+print("a é igual a b?", a == b)
+print("b é maior que a?", b > a)
+print("a é menor que b?", a < b)
+print("a é igual a d?", a == d)
+print("b é maior ou igual a a?", b >= a)
+print("c é menor ou igual a b?", c <= b)
+print("d é diferente de a?", d != a)
+print("d é diferente de b?", d != b)
+
+#Exemplo do uso de operadores relacionais com variáveis do tipo lógico
+nota = 8
+media = 7
+aprovado = nota > media
+print(aprovado)
