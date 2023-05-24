704ad6fd1923264f4f8bd4e00351ee052bc9cdff
tatiuska <tfweska@gmail.com>
2023-05-24 15:54:20 -0300

    exercício sobre operadores relacionais e lógicos

---
+++ ex003.py
@@ -0,0 +1,18 @@
+#Exercício 3.2: Complete a tabela abaixo, respondendo True ou False. Considere a = 4, b = 10, c = 50, d = 1 e f = 5
+a = 4
+b = 10
+c = 50
+d = 1
+f = 5
+
+print("a é igual a c?", a == c)
+print("a é menor que b?", a < b)
+print("d é maior que b?", d > b)
+print("c é diferente de f?", c != f)
+print("a é igual a b?", a == b)
+print("c é menor que d?", c < d)
+print("b é maior que a?", b > a)
+print("c é maior ou igual a f?", c >= f)
+print("f é maior ou igual a c?", f >= c)
+print("c é menor ou igual a c?", c <= c)
+print("f é menor ou igual a c?", f <= c)
