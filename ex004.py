e0e7581191e105f0e71045701f472cfaf60018e6
tatiuska <tfweska@gmail.com>
2023-05-24 16:38:02 -0300

    exercício para operadores lógicos

---
+++ ex004.py
@@ -0,0 +1,18 @@
+#Exercício 3.3: Complete a tabela a seguir utilizando a = True, b = False e c = True
+a = True
+b = False
+c = True
+
+print("a and a:", a and a)
+print("b and b:", b and b)
+print("not c:", not c)
+print("not b:", not b)
+print("not a:", not a)
+print("a and b:", a and b)
+print("b and c:", b and c)
+print("a or c:", a or c)
+print("b or c:", b or c)
+print("c or a:", c or a)
+print("a or b:", a or b)
+print("c or c:", c or c)
+print("b or b:", b or b)
