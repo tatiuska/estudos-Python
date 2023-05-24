e7298ae13751d39464e15b86e22fc01455895234
tatiuska <tfweska@gmail.com>
2023-05-24 18:13:40 -0300

    exercício operadores lógicos

---
+++ ex006.py
@@ -0,0 +1,18 @@
+#Exercício 3.5: Calcular o resultado da expressão A > B and C or D, utlizando os valores apresentados na tabela do livro
+A = 1
+B = 2
+C = True
+D = False
+print("A > B and C or D:", A > B and C or D)
+
+A = 10
+B = 3
+C = False
+D = False
+print("A > B and C or D:", A > B and C or D)
+
+A = 5
+B = 1
+C = True
+D = True
+print("A > B and C or D:", A > B and C or D)
