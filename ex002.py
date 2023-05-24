190febfbaba7a117d5ee490e00cf34a7b5972bb0
tatiuska <tfweska@gmail.com>
2023-05-24 00:11:40 -0300

    exercícios com variáveis

---
+++ ex002.py
@@ -0,0 +1,19 @@
+#Exercício 2.3: Faça um programa que exiba seu nome na tela
+nome = "Tatiana"
+print("Meu nome é", nome)
+
+#Exercício 2.4: Escreva um programa que exiba o resultado de 2a x 3b, onde a vale 3 e b vale 5
+a = 3
+b = 5
+print("2a * 3b =", (2*a) * (3*b))
+
+#Exercício 2.5: Modifique o primeiro programa, listagem 2.7, de forma a calcular a soma de três variáveis
+a = 2
+b = 3
+c = 7
+print("a + b + c =", a + b + c)
+
+#Exercício 2.6: Modificar o programa 2.11 para calcular o aumento de 15% para um salário de R$ 750
+salario = 750
+aumento = 15
+print("R$", salario + (salario * aumento / 100))
