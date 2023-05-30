d85042178d4842efb729f4f4767a0cd4140f3e18
tatiuska <tfweska@gmail.com>
2023-05-30 20:29:45 -0300

    Uso da função len()

---
+++ strings.py
@@ -0,0 +1,6 @@
+#Capítulo 3.4 - Variáveis string
+#Uso da função len()
+print("Número de caracteres na string 'A':", len("A"))
+print("Número de caracteres na string 'AB':", len("AB"))
+print("Número de caracteres na string vazia:", len(""))
+print("Número de caracteres na string 'O rato roeu a roupa':", len("O rato roeu a roupa"))
