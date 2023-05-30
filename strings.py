ee79eaf759fdba5e54e6dc3eb4faa93faf117492
tatiuska <tfweska@gmail.com>
2023-05-30 20:31:56 -0300

    Adição de comentário no arquivo strings.py

--- strings.py
+++ strings.py
@@ -10,5 +10,5 @@ a = "ABCDEF"
 print("Na string 'ABCDE', o caracter no índice 0 é:", a[0])
 print("Na string 'ABCDE', o caracter no índice 1 é:", a[1])
 print("Na string 'ABCDE', o caracter no índice 5 é:", a[5])
-print("Na string 'ABCDE', o caracter no índice 6 é:", a[6])
+print("Na string 'ABCDE', o caracter no índice 6 é:", a[6]) #Aqui vai dar erro
 print("O tamanho da string 'ABCDEF' é:", len(a))
