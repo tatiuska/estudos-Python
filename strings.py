23d516285da9cc36fd9470119cbe9690107a75e8
tatiuska <tfweska@gmail.com>
2023-05-30 20:30:48 -0300

    Prática com índices em strings

--- strings.py
+++ strings.py
@@ -4,3 +4,11 @@ print("Número de caracteres na string 'A':", len("A"))
 print("Número de caracteres na string 'AB':", len("AB"))
 print("Número de caracteres na string vazia:", len(""))
 print("Número de caracteres na string 'O rato roeu a roupa':", len("O rato roeu a roupa"))
+
+#Testes com strings
+a = "ABCDEF"
+print("Na string 'ABCDE', o caracter no índice 0 é:", a[0])
+print("Na string 'ABCDE', o caracter no índice 1 é:", a[1])
+print("Na string 'ABCDE', o caracter no índice 5 é:", a[5])
+print("Na string 'ABCDE', o caracter no índice 6 é:", a[6])
+print("O tamanho da string 'ABCDEF' é:", len(a))
