545110b81734aaa5961d92d14f5e7b67cf5aa7f9
tatiuska <tfweska@gmail.com>
2023-06-07 22:57:55 -0300

    exercício 3.7

---
+++ 2023-06-07_ex3-7.py
@@ -0,0 +1,6 @@
+#Exercício 3.7 - Programa que peça dois números inteiros e imprima a soma deles.
+x = int(input("Insira um número: "))
+y = int(input("Insira outro número: "))
+
+print(f"A soma entre {x} e {y} é igual a {x + y}")
+
