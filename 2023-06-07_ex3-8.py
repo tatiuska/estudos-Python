45f83a0d0a02086b72c4bfe0ba81d8ff87471f6b
tatiuska <tfweska@gmail.com>
2023-06-07 23:06:44 -0300

    exercício 3.8

---
+++ 2023-06-07_ex3-8.py
@@ -0,0 +1,3 @@
+#Exercício 3.8: Escreva um programa que leia um valor em metros e o exiba convertido em milímetros
+x = int(input("Digite um valor: "))
+print(f"{x} metros equivalem a {x * 1000} milímetros")
