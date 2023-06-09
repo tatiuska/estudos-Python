f1c0c1f2b55b2b4ad2940d4f3de7b385d6a48c2d
tatiuska <tfweska@gmail.com>
2023-06-08 22:00:14 -0300

    Exercício 04 Reunião 08/06/23

---
+++ 2023-06-08_ex04.py
@@ -0,0 +1,9 @@
+# Exercício 4:
+# Crie um dicionário chamado "alunos" com as seguintes chaves e valores: "João" - 8, "Maria" - 9, "Pedro" - 7.
+# Utilizando um loop for, calcule e imprima a média das notas dos alunos.
+
+alunos = {"João": 8, "Maria": 9, "Pedro": 7}
+media = 0
+for nota in alunos.values():
+    media = media + nota / 3
+print(media)
