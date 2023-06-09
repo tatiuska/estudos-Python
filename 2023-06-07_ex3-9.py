c7055930a7d90b65b8896d5b055ab42153063d28
tatiuska <tfweska@gmail.com>
2023-06-08 21:33:07 -0300

    Exercício 3.9

---
+++ 2023-06-07_ex3-9.py
@@ -0,0 +1,6 @@
+#Exercício 3.9: Escrever um programa que leia a quantidade de dias, horas, minutos e segundos do usuário.
+#Calcular o total em segundos.
+idade = input("Informe sua idade: ")
+
+
+input(f"a data de hoje é {data}")
