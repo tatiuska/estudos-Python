92220915f274e9ee2a9f066c4a21d7d158a06530
tatiuska <tfweska@gmail.com>
2023-06-08 21:14:29 -0300

    Exercício 01 Reunião 08/06/23

---
+++ 2023-06-08_ex01.py
@@ -0,0 +1,11 @@
+#Exercício 01 - do chat GPT
+#Crie um dicionário chamado "frutas" com as seguintes chaves e valores:
+# Maçã - 3
+# Banana - 6
+# Laranja - 4
+#Imprima o valor associado à chave Banana
+
+frutas = {"Maçã": 3, "Banana": 6, "Laranja": 4}
+print(frutas["Banana"])
+
+
