637dd25a3d1b04902f2cbbbce490776b2d9efaef
tatiuska <tfweska@gmail.com>
2023-06-13 20:42:44 -0300

    Exercício 07 (lista de 08/06/2023)

---
+++ 2023-06-08_ex07.py
@@ -0,0 +1,13 @@
+# Exercício 7:
+# Crie um dicionário chamado "agenda" com os seguintes pares de chaves e valores: "Ana" - "555-1234",
+# "Maria" - "555-5678", "João" - "555-9876". Solicite ao usuário que digite um nome e, em seguida, verifique se o
+# nome está presente na agenda. Se estiver, imprima o número de telefone correspondente; caso contrário, exiba uma
+# mensagem informando que o nome não foi encontrado.
+
+agenda = {'Ana': '555-1234', 'Maria': '555-5678', 'João': '555-9876'}
+nome = input("Digite um nome: ")
+if nome in agenda.keys():
+    for numero in agenda.values():
+        print(f"{numero}")
+else:
+    print("Nome não encontrado!")
