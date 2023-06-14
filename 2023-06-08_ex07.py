bf673dbaa473ff438dc4cf1f4e8284bacca5098d
tatiuska <tfweska@gmail.com>
2023-06-13 21:29:48 -0300

    correção exercício 07

--- 2023-06-08_ex07.py
+++ 2023-06-08_ex07.py
@@ -7,7 +7,6 @@
 agenda = {'Ana': '555-1234', 'Maria': '555-5678', 'João': '555-9876'}
 nome = input("Digite um nome: ")
 if nome in agenda.keys():
-    for numero in agenda.values():
-        print(f"{numero}")
+    print(f"{agenda[nome]}")
 else:
     print("Nome não encontrado!")
