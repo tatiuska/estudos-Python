a2ce95b8cbf203b950d8d48f80fc2a5d4c7b01ba
tatiuska <tfweska@gmail.com>
2023-06-19 22:02:39 -0300

    Exercício 01 da reunião de 19/06/2023

---
+++ 2023-06-19_chatGPT_ex01.py
@@ -0,0 +1,4 @@
+# Exercício 1:
+# Crie um conjunto chamado "frutas" com os nomes de algumas frutas. Imprima o conjunto para visualizar os elementos.
+frutas = {"banana", "amora", "pitanga", "pitaya", "jaboticaba", "manga"}
+print(frutas)
