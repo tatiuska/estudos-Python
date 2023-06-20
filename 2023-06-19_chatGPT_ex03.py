567ba5db1c9286e54f8f3d71da4811a487915b45
tatiuska <tfweska@gmail.com>
2023-06-19 23:07:09 -0300

    Exercício 03 da reunião de 19/06/2023

---
+++ 2023-06-19_chatGPT_ex03.py
@@ -0,0 +1,6 @@
+# Exercício 3:
+# Crie dois conjuntos chamados "A" e "B" com alguns elementos. Calcule e imprima a interseção dos dois conjuntos.
+A = {"Elaine", "Jerry", "sopa", "George", "Kramer"}
+B = {"sopa", "pretzel", "sucrilhos", "bagel", "Elaine"}
+
+print(A.intersection(B))
