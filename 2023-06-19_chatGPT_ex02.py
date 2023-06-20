de33ccc86f3b37f7b7f87d23f1f7db4f058602f6
tatiuska <tfweska@gmail.com>
2023-06-19 22:11:33 -0300

    Exercício 02 da reunião de 19/06/2023

---
+++ 2023-06-19_chatGPT_ex02.py
@@ -0,0 +1,6 @@
+# Exercício 2:
+# Crie dois conjuntos chamados "A" e "B" com alguns elementos. Calcule e imprima a união dos dois conjuntos.
+A = {"Elaine", "Jerry", "George", "Kramer"}
+B = {"sopa", "pretzel", "sucrilhos", "bagel"}
+
+print(A | B)
