fc4b91e275a302855b297d860ba179986779a946
tatiuska <tfweska@gmail.com>
2023-06-20 22:05:32 -0300

    Exercício 5 da reunião de 20/06/2023

---
+++ 2023-06-20_chatGPT_ex05.py
@@ -0,0 +1,6 @@
+# Exercício 5:
+# Dada uma lista de números, imprima apenas os números pares usando um loop for.
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for i in numeros:
+    if (i % 2) == 0:
+        print(i)
