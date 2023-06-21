feed8ad86e9cc35d6ce63fde4ed170be76385420
tatiuska <tfweska@gmail.com>
2023-06-20 21:52:44 -0300

    Exercícios 1 e 2 da reunião de 20/06/2023

---
+++ 2023-06-20_chatGPT_ex01.py
@@ -0,0 +1,4 @@
+# Exercício 1:
+# Imprima os números de 1 a 10 usando um loop for.
+for i in range(1, 11):
+    print(i)
---
+++ 2023-06-20_chatGPT_ex02.py
@@ -0,0 +1,6 @@
+# Exercício 2:
+# Dado um lista de nomes, imprima cada nome em uma linha separada usando um loop for.
+nomes = ["Jerry", "Elaine", "Kramer", "George"]
+
+for nome in nomes:
+    print(nome)
