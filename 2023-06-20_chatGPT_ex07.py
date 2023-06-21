5afdc3cc1dc9ff5d66364466e646e777818d64e1
tatiuska <tfweska@gmail.com>
2023-06-20 22:35:15 -0300

    Exercício 7 da reunião de 20/06/2023

---
+++ 2023-06-20_chatGPT_ex07.py
@@ -0,0 +1,4 @@
+# Dada uma lista de números, calcule e imprima o quadrado de cada número utilizando um loop for.
+numeros = [2, 34, 56, 5, 19, 230]
+for i in numeros:
+    print(f"{i} ** {2} = {i ** 2}")
