c8b26d2cb80e037c17c3e2501d0bb9d3ad143893
tatiuska <tfweska@gmail.com>
2023-06-22 22:28:39 -0300

    Exercício 7.4 da reunião de 22/06/2023

---
+++ 2023-06-22_livro_ex7-4.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia uma string e imprima quantas vezes cada caractere aparece nessa string.
+s = "TTAAC"
+print(f"Número de vezes que aparece a letra T na string {s}: {s.count('T')}")
+print(f"Número de vezes que aparece a letra A na string {s}: {s.count('A')}")
+print(f"Número de vezes que aparece a letra C na string {s}: {s.count('C')}")
