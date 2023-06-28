a6bbd9eba79042d5694e2d7b6d96f8fed04c7d5d
tatiuska <tfweska@gmail.com>
2023-06-27 21:17:44 -0300

    Exercício 01 Reunião 27/06/2023

---
+++ 2023-06-27_chatGPT_ex01.py
@@ -0,0 +1,6 @@
+# Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+raio = float(input("Informe o valor do raio: "))
+pi = 3.14
+
+area = pi * (raio ** 2)
+print(f"A área do círculo é: {area}")
