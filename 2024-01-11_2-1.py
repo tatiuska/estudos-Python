c6d9990f94e69b743b697b96638311f22ca6e698
tatiuska <tfweska@gmail.com>
2024-01-11 22:14:21 -0300

    exercícios Encontro Grupo 2 (11-01-2024)

---
+++ 2024-01-11_2-1.py
@@ -0,0 +1,13 @@
+# exercícios Capítulo 2
+# 2.1 - Converta as seguintes expressões matemáticas para que possam ser
+# calculadas usando o interpretador Python
+
+resultado1 = 10+20*30
+print(resultado1)
+
+resultado2 = 4**2+30
+print(resultado2)
+
+resultado3 = (9**4+2)*6-1
+print(resultado3)
+
---
+++ 2024-01-11_2-2.py
@@ -0,0 +1,4 @@
+# 2.2 Digite a seguinte expressão no interpretador
+
+resultado = 10%3*10**2+1-10*4/2
+print(resultado)
