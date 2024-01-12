71c18da7add176229df17a2f0243e2a350438683
tatiuska <tfweska@gmail.com>
2024-01-11 22:17:09 -0300

    conteúdo variáveis encontro Grupo 2 (11-01-2024)

---
+++ 2024-01-11_variaveis.py
@@ -0,0 +1,14 @@
+# variáveis
+# b = 15
+# h = 7
+# perimetro = (2*b)+(2*h)
+# area = b*h
+# print(perimetro, area)
+
+# Programa 2.2
+salario = 2000
+aumento = 7
+print(salario + (salario * aumento / 100))
+
+# Programa 2.3
+# print(1500 + (1500 * 5 / 100))
