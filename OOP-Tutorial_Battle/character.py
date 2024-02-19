f32156bab811f04cff911afa5f24eb5d03df7d77
tatiuska <tfweska@gmail.com>
2024-02-19 19:32:12 -0300

    modificação classe Character e criação sub-classes Hero e Enemy

--- OOP-Tutorial_Battle/character.py
+++ OOP-Tutorial_Battle/character.py
@@ -1,16 +1,30 @@
 # Character class
+from weapon import fists
+
+
 class Character:
     # Class-level variables
     # race = "Human"
 
-    def __init__(self, name: str, health: int, damage: int):
+    def __init__(self, name: str, health: int):
         # Object-level variables
         self.name = name
         self.health = health
         self.health_max = health
-        self.damage = damage
+
+        self.weapon = fists
 
     # Method Attack
     def attack(self, target) -> None:
-        target.health -= self.damage
+        target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+
+
+class Hero(Character):
+    def __init__(self, name: str, health: int) -> None:
+        super().__init__(name=name, health=health)
+
+
+class Enemy(Character):
+    def __init__(self, name: str, health: int) -> None:
+        super().__init__(name=name, health=health)
