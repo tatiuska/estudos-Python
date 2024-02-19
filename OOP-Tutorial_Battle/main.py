e048b15f2842da37eef55525e1551b89a6df8d1b
tatiuska <tfweska@gmail.com>
2024-02-19 20:48:36 -0300

    ajustes main.py

--- OOP-Tutorial_Battle/main.py
+++ OOP-Tutorial_Battle/main.py
@@ -1,7 +1,10 @@
 from character import Hero, Enemy
+from weapon import short_bow, iron_sword
 
 hero = Hero(name="Hero", health=100)
-enemy = Enemy(name="Enemy", health=100)
+hero.equip(iron_sword)
+
+enemy = Enemy(name="Enemy", health=100, weapon=short_bow)
 
 while True:
     hero.attack(enemy)
@@ -10,4 +13,5 @@ while True:
     print(f"Health of {hero.name}: {hero.health}")
     print(f"Health of {enemy.name}: {enemy.health}")
 
+    hero.drop()
     input()
