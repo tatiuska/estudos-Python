729db1b0ddfb6d76ae56364ab6fb01d3ceadaf24
tatiuska <tfweska@gmail.com>
2024-02-19 20:46:14 -0300

    ajustes função drop

--- OOP-Tutorial_Battle/character.py
+++ OOP-Tutorial_Battle/character.py
@@ -18,13 +18,29 @@ class Character:
     def attack(self, target) -> None:
         target.health -= self.weapon.damage
         target.health = max(target.health, 0)
+        print(f"{self.name} dealt {self.weapon.damage} damage to {target.name} with {self.weapon.name}.")
 
 
 class Hero(Character):
     def __init__(self, name: str, health: int) -> None:
         super().__init__(name=name, health=health)
 
+        self.default_weapon = self.weapon
+
+    def equip(self, weapon) -> None:
+        self.weapon = weapon
+        print(f"{self.name} equipped a(n) {self.weapon.name}!")
+
+    def drop(self) -> None:
+        if self.weapon == self.default_weapon:
+            print(f"{self.name} lowered his {self.default_weapon.name}!")
+        else:
+            print(f"{self.name} dropped the {self.weapon.name}!")
+            self.weapon = self.default_weapon
+
+
 
 class Enemy(Character):
-    def __init__(self, name: str, health: int) -> None:
+    def __init__(self, name: str, health: int, weapon) -> None:
         super().__init__(name=name, health=health)
+        self.weapon = weapon
