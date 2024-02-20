f618a4b5a4ca4cfdf0ab5999cc538c6be51b478c
tatiuska <tfweska@gmail.com>
2024-02-20 20:11:47 -0300

    ajustes wordle.py

--- wordle/wordle.py
+++ wordle/wordle.py
@@ -8,9 +8,12 @@ class Wordle:
         self.attempts = []
         pass
 
+    def attempt(self, word: str):
+        self.attempts.append(word)
+
     @property
     def is_solved(self):
-        return self.attempts[-1] == self.secret
+        return len(self.attempts) > 0 and self.attempts[-1] == self.secret
 
     @property
     def remaining_attempts(self) -> int:
@@ -18,4 +21,4 @@ class Wordle:
 
     @property
     def can_attempt(self):
-        return self.remaining_attempts == 0 and not self.is_solved
+        return self.remaining_attempts > 0 and not self.is_solved
