d33e4d5edd4af22b91336055c4ab36b0b4710d73
tatiuska <tfweska@gmail.com>
2024-02-20 20:26:00 -0300

    correção typo

--- wordle/letter_state.py
+++ wordle/letter_state.py
@@ -1,6 +1,5 @@
 class LetterState:
     def __init__(self, character: str):
         self.character: str = character
-        self.is_in_world: bool = False
+        self.is_in_word: bool = False
         self.is_in_position: bool = False
-        
