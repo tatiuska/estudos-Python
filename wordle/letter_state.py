95c909b6204c5507579cba613f8b5e823619eb24
tatiuska <tfweska@gmail.com>
2024-02-20 20:18:22 -0300

    criação classe LetterState, que irá verificar se os caracteres fazem parte da palavra-chave e se estão na posição certa

---
+++ wordle/letter_state.py
@@ -0,0 +1,6 @@
+class LetterState:
+    def __init__(self, character: str):
+        self.character: str = character
+        self.is_in_world: bool = False
+        self.is_in_position: bool = False
+        
