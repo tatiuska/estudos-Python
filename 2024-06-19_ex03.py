bde494c47974557b13dd5ae103820161ccdb18bc
tatiuska <tfweska@gmail.com>
2024-06-26 20:58:13 -0300

    exercícios de funções e POO (classes) da lista do dia 14/06/2024 - 3

--- 2024-06-19_ex03.py
+++ 2024-06-19_ex03.py
@@ -3,12 +3,27 @@
 # e False caso contrário.
 
 
+# Função para saber se o número é par
 def eh_par():
+    # Variável vai receber o input, tipado em int
     numero = int(input("Digite um número: "))
+
+    # Aplicação da etsrutura condicional if/else
     if numero % 2 == 0:
         print(f"O número {numero} é par!")
     else:
         print(f"O número {numero} é ímpar!")
 
 
+# Chamando a função
 eh_par()
+
+
+# # versão 2
+# def eh_par(x: int) -> bool:
+#     if x % 2:
+#         return False
+#     else:
+#         return True
+#
+# print(eh_par(44))
